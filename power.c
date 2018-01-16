a0f01aa4d19621aaad9463345be0d431b459a568
AgrawalPragati <35077511+AgrawalPragati@users.noreply.github.com>
2018-01-16 22:47:01 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int n,e,power;
+	printf("Enter element and power:\n");
+	scanf("%d%d",&n,&e);
+    power=pow(n,e);
+
+	
+  printf("Power is %d",power);
+}
