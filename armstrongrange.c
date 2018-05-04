39eabcba596d73320a52ba953d384b23c9f44688
AgrawalPragati <35077511+AgrawalPragati@users.noreply.github.com>
2018-05-05 00:56:57 +0530

    Add files via upload

---
+++ armstrongrange.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int n,temp,p,a,b;
+	scanf("%d%d",&a,&b);
+	for(n=a;n<=b;n++)
+	{
+	temp=n;
+	p=n;
+	int count=0,rem=0,sum=0;
+	while(temp!=0)
+	{
+	count++;
+	temp=temp/10;	
+	}
+	while(p!=0)
+	{
+	rem=p%10;
+    sum=sum+pow(rem,count);
+	p=p/10;	
+	}
+	if(sum==n)
+	{
+		printf("%d\n" ,sum);
+	}
+	sum=0;
+}}
